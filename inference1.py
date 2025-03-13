a13752b9f8f183dd7cc1b5d5fdc31dde26d10a3c
ceastld <1950946176@qq.com>
2025-03-13 13:59:23 +0000

    fix inference1

--- inference1.py
+++ inference1.py
@@ -1,13 +1,13 @@
 import numpy as np
 import cv2
 from latentsync.inference.lipsync_model import LipsyncModel, LipsyncContext
-from latentsync.pipelines.lipsync_pipeline import LipsyncMetadata
+from latentsync.pipelines.lipsync_diffusion_pipeline import LipsyncMetadata
 import torch
 from tqdm import tqdm
 
 from latentsync.utils.util import read_audio
-from latentsync.utils.video import save_frames_to_video
-
+from latentsync.utils.video import save_frames_to_video, video_stream
+from latentsync.whisper.whisper.audio import load_audio
 
 def run_inference(video_path: str, audio_path: str, output_path: str):
     # Initialize model and context
@@ -20,7 +20,8 @@ def run_inference(video_path: str, audio_path: str, output_path: str):
     cap = cv2.VideoCapture(video_path)
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    audio_samples = read_audio(audio_path)
+    audio_samples = load_audio(audio_path)
+
     num_audio_clips = (
         np.ceil(audio_samples.shape[0] / (16000 / 25) / batch_size) * batch_size
     )
@@ -31,7 +32,6 @@ def run_inference(video_path: str, audio_path: str, output_path: str):
     )
     total_frames = min(total_frames, num_audio_clips)
 
-    metadata_list = []
     processed_frames = []
 
     frame_idx = 0
@@ -41,6 +41,9 @@ def run_inference(video_path: str, audio_path: str, output_path: str):
         batch_metadata = []
         frames_in_batch = 0
         for _ in range(batch_size):
+            if frame_idx + frames_in_batch >= total_frames:
+                break
+                
             ret, frame = cap.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -80,7 +83,7 @@ def run_inference(video_path: str, audio_path: str, output_path: str):
 
 if __name__ == "__main__":
     # Example usage
-    video_path = "assets/obama.mp4"
+    video_path = "assets/obama1.mp4"
     audio_path = "assets/cxk.mp3"
     output_path = "video_out1.mp4"
 
